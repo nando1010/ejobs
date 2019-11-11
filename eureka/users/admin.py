e4516a8340353c813153c2104cd81bdc85b65921
Fernando Segura <fsegura.agreda@gmail.com>
2019-11-11 15:24:24 -0500

    Incluye la version revisada de Admin para Profile, User, _CreateUser, Create User, SuperUser

--- eureka/users/admin.py
+++ eureka/users/admin.py
@@ -2,17 +2,22 @@
 
 # Django
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 #Models
 from eureka.users.models import User,Profile
 
-
-class CustomUserAdmin(UserAdmin):
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
     """User model admin."""
 
-    list_display = ('email','username','first_name','last_name','is_staff','is_client')
+    list_display = ('email','first_name','last_name','is_staff','is_client')
     list_filter =('is_client','is_staff','created','modified')
+    readonly_fields=[
+        'created',
+        'last_login',
+        'modified'
+    ]
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
@@ -20,6 +25,3 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user','jobs_applied','jobs_created','active_search')
     search_fields = ('user__email','user__first_name','user__last_name')
     list_filter = ('active_search',)
-
-
-admin.site.register(User,CustomUserAdmin)
--- eureka/users/models/users.py
+++ eureka/users/models/users.py
@@ -2,9 +2,8 @@
 
 # Django
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin,AbstractBaseUser,BaseUserManager
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import BaseUserManager
 
 # Utilities
 from eureka.utils.models import EurekaModel
@@ -15,35 +14,62 @@ class MyUserManager(BaseUserManager):
     instead of usernames. The default that's used is "UserManager"
     """
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email, password, first_name, last_name, **extra_fields):
         """
-        Creates and saves a User with the given email and password.
+        Internal Method for Creating and saving a User with the given email and password.
         """
         if not email:
             raise ValueError('The Email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        email=self.normalize_email(email)
+        user = self.model(
+            email=email,
+            password = password,
+            first_name = first_name,
+            last_name=last_name
+        )
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+    def create_user(self, email, password, first_name, last_name,phone_number,**extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        user = self._create_user(email=email, password=password,first_name=first_name,last_name=last_name,**extra_fields)
+        user.phone_number=phone_number
+        user.is_staff= False
+        user.is_superuser=False
+        user.save()
+        return user
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(email, password, **extra_fields)
+    def create_superuser(self, email, password, first_name, last_name,**extra_fields):
+        """
+        Creates and saves a SuperUser with the given email and password.
+        """
+        user = self._create_user(email=email, password=password,first_name=first_name,last_name=last_name,**extra_fields)
+        user.is_staff= True
+        user.is_superuser=True
+        user.save()
+        return user
 
 
-class User(EurekaModel,AbstractUser):
+    def get_by_natural_key(self,email_):
+        print(email_)
+        return self.get(email=email_)
+
+class User(EurekaModel,AbstractBaseUser,PermissionsMixin):
     """User model.
+    Here we are subclassing the Django AbstractBaseUser, which comes with only
+    3 fields:
+    1 - password
+    2 - last_login
+    3 - is_active
+    Note than all fields would be required unless specified otherwise, with
+    `required=False` in the parentheses.
 
-    Extend from Django's AbstractUser, change the username field
-    to email and add some extra fields.
+    The PermissionsMixin is a model that helps you implement permission settings
+    as-is or modified to your requirements.
+    More info: https://goo.gl/YNL2ax
     """
 
     email = models.EmailField(
@@ -52,6 +78,16 @@ class User(EurekaModel,AbstractUser):
         error_messages={
             'unique': 'A user with that email already exists.'
         }
+    )
+
+    first_name = models.CharField(
+        max_length=30,
+        verbose_name='first name'
+    )
+
+    last_name=models.CharField(
+        max_length=150,
+        verbose_name='last name'
     )
 
     phone_regex = RegexValidator(
@@ -69,6 +105,18 @@ class User(EurekaModel,AbstractUser):
     REQUIRED_FIELDS = ['first_name','last_name']
     objects = MyUserManager()
 
+    is_staff = models.BooleanField(
+        default=False,
+        help_text='Designates whether the user can log into this admin site.',
+        verbose_name='staff status'
+    )
+
+    is_superuser = models.BooleanField(
+        default=False,
+        help_text='Designates whether the user can have superuser attributes.',
+        verbose_name='superuser status'
+    )
+
     is_client = models.BooleanField(
         'client',
         default = True,
@@ -84,10 +132,13 @@ class User(EurekaModel,AbstractUser):
         help_text = 'Set to true when the user have verified its email adress.'
     )
 
-    def __str__(self):
+    def __get_short_name(self):
         """Return email."""
         return self.email
 
-    def __get_short_name(self):
+    def natural_key(self):
+        return self.email
+
+    def __str__(self):
         """Return email."""
         return self.email
